eeb3b81e48239e5689c144fbcb68c72e4ef56cb5
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-04-17 11:49:50 +0300

    message

---
+++ src/app/main/main.module.js
@@ -0,0 +1,6 @@
+
+
+
+angular.module( 'angularTDDSample.main', [
+  'ui.router'
+]);
---
+++ src/app/main/main.route.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+angular.module( 'angularTDDSample.main')
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'main', {
+    url: '/main',
+    views: {
+      "main": {
+        controller: 'MainCtrl',
+        templateUrl: 'main/main.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Main' }
+  });
+});
