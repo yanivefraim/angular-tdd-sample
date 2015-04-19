c9157c7fc3219ba9329452ce06453cc7fe8d4d69
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-04-19 17:25:34 +0300

    small refactor

--- src/app/main/main.module.js
+++ src/app/main/main.module.js
@@ -1,6 +1,7 @@
 
-
 (function(){
+  'use strict';
+
   angular.module( 'angularTDDSample.main', [
     'ui.router'
   ]);
--- src/app/main/main.route.js
+++ src/app/main/main.route.js
@@ -1,18 +1,20 @@
 
 
 
+(function(){
+  angular
+      .module( 'angularTDDSample.main')
+      .config(function config( $stateProvider ) {
+        $stateProvider.state( 'main', {
+          url: '/main',
+          views: {
+            "main": {
+              controller: 'MainCtrl',
+              templateUrl: 'main/main.tpl.html'
+            }
+          },
+          data:{ pageTitle: 'Main' }
+        });
+      });  
+}());
 
-
-angular.module( 'angularTDDSample.main')
-.config(function config( $stateProvider ) {
-  $stateProvider.state( 'main', {
-    url: '/main',
-    views: {
-      "main": {
-        controller: 'MainCtrl',
-        templateUrl: 'main/main.tpl.html'
-      }
-    },
-    data:{ pageTitle: 'Main' }
-  });
-});
