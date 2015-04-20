5d3694f6403690fa8e9c50754a4bdb2f0708f660
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-04-20 16:40:46 +0300

    added itemList directive

--- src/components/item-list/item-list.directive.js
+++ src/components/item-list/item-list.directive.js
@@ -1,15 +1,36 @@
 
 (function(){
 	
+	'use strict';
+
+	function itemList() {
+		
+		function itemListCtrl () {
+
+		}
+
+		function link () {
+
+  	}
+
+		return {
+		  restrict: 'E',
+		  scope: {},
+		  template: [
+		    '<div>',
+		    '</div>'
+		  ].join(''),
+		  controllerAs: 'vm',
+		  controller: itemListCtrl,
+		  link: link
+		};
+	}
+
 	angular
 		.module('angularTDDSample.itemList',[]);
 
 	angular
 		.module('angularTDDSample.itemList')
 		.directive('itemList', itemList);
-
-	function itemList() {
-
-	}
 
 }());
