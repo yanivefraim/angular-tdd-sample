5d93a2b8962c71572195a0a0ea3215c08bcf5bd7
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-04-21 22:40:34 +0300

    tests

--- src/components/item-list/item-list.spec.js
+++ src/components/item-list/item-list.spec.js
@@ -1,24 +1,56 @@
 describe('Unit testing item-list directive', function() {
   var $compile,
-      $rootScope;
+      $rootScope,
+      scope,
+      defaultItemsJSON,
+      element;
 
   // Load the angularTDDSample.itemList module, which contains the directive
-  beforeEach(module('angularTDDSample.itemList'));
+  beforeEach(module('angularTDDSample.itemList', 'angularTDDSample.item.mock'));
 
   // Store references to $rootScope and $compile
   // so they are available to all tests in this describe block
-  beforeEach(inject(function(_$compile_, _$rootScope_){
+  beforeEach(inject(function(_$compile_, _$rootScope_, _defaultItemsJSON_){
     // The injector unwraps the underscores (_) from around the parameter names when matching
     $compile = _$compile_;
+    
     $rootScope = _$rootScope_;
+
+    defaultItemsJSON = _defaultItemsJSON_;
+
+    scope = $rootScope.$new();
+
+    scope.defaultItemsJSON = defaultItemsJSON.data;
+  
+    element = angular.element('<item-list items="defaultItemsJSON"></item-list>');
+
+    // Compile a piece of HTML containing the directive
+    $compile(element)(scope);
+
+    scope.$digest();
+    
   }));
 
-  it('Replaces the element with the appropriate content', function() {
-    // Compile a piece of HTML containing the directive
-    var element = $compile("<item-list></item-list>")($rootScope);
-    // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
-    $rootScope.$digest();
-    // Check that the compiled element contains the templated content
-    //expect(element.html()).toContain("lidless, wreathed in flame, 2 times");
+  it('Expect "items" to be defined', function() {
+    
+    var isolated = element.isolateScope();
+    
+    expect(isolated.items.length).toBeDefined();
+
+  });
+
+  it('Expect "items" to have 100 items', function(){
+    var isolated = element.isolateScope();
+    
+    expect(isolated.items.length).toBe(100);    
+  });
+
+  it('Expect element to have items attr', function(){
+    expect(element.attr('items')).toBeDefined();
+  });
+
+  it('Expect element to have items attr', function(){
+    var li = element.find('li'); 
+    expect(li.length).toBe(100);
   });
 });
--- src/models/item/item.spec.js
+++ src/models/item/item.spec.js
@@ -30,7 +30,8 @@ describe('Service: item', function () {
     expect(item.getList).toBeDefined();
   });
  
-  it('should get list-items', inject(function () {
+  //TODO: how can I 
+  it('should get list-items', function () {
       var items;
       //given
       httpBackend.whenGET("http://www.mocky.io/v2/5535581b57bdef59003799bc").respond(defaultJSON);
@@ -43,7 +44,25 @@ describe('Service: item', function () {
       httpBackend.flush();
 
       //then
-      expect(items).toEqual(defaultJSON.data);      
+      expect(items.length).toBe(100);//toEqual(defaultJSON.data); 
+
+      expect(items.map(function(elm){return elm.first_name;})).toEqual(defaultJSON.data.map(function(elm){return elm.first_name;}));//toEqual(defaultJSON.data);      
       
-  }));
+  });
+
+  it('should have correct first_names', function(){
+    var items;
+      //given
+      httpBackend.whenGET("http://www.mocky.io/v2/5535581b57bdef59003799bc").respond(defaultJSON);
+
+      //when
+      item.getList().then(function(response){
+        items = response.data;
+      });
+
+      httpBackend.flush();
+
+      //then
+      expect(items.map(function(elm){return elm.first_name;})).toEqual(defaultJSON.data.map(function(elm){return elm.first_name;}));//toEqual(defaultJSON.data);      
+  });
 });
