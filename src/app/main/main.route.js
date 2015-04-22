2d6b084156af6ffa80f7a52a41d0d210bb32ad54
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-04-22 11:09:52 +0300

    temporary remove route module definition

--- src/app/main/main.route.js
+++ src/app/main/main.route.js
@@ -1,29 +1,29 @@
 
 
 
-(function(){
+// (function(){
 
-  // function getItems (item) {
-  //   return item.getList();
-  // }; 
+//   // function getItems (item) {
+//   //   return item.getList();
+//   // }; 
 
-  angular
-      .module('angularTDDSample.main')
-      // .config(function config( $stateProvider ) {
-      //   $stateProvider.state( 'main', {
-      //     url: '/main',
-      //     views: {
-      //       "main": {
-      //         controller: 'MainCtrl',
-      //         controllerAs: 'vm',
-      //         templateUrl: 'main/main.tpl.html',
-      //         resolve: {
-      //           itemListData: getItems
-      //         }
-      //       }
-      //     },
-      //     data:{ pageTitle: 'Main' }
-      //   });
-      // });  
-}());
+//   angular
+//       .module('angularTDDSample.main')
+//       // .config(function config( $stateProvider ) {
+//       //   $stateProvider.state( 'main', {
+//       //     url: '/main',
+//       //     views: {
+//       //       "main": {
+//       //         controller: 'MainCtrl',
+//       //         controllerAs: 'vm',
+//       //         templateUrl: 'main/main.tpl.html',
+//       //         resolve: {
+//       //           itemListData: getItems
+//       //         }
+//       //       }
+//       //     },
+//       //     data:{ pageTitle: 'Main' }
+//       //   });
+//       // });  
+// }());
 
